b7b8c6d859c0d2f2670b651fd98bc3f7120db4b0
Dolsy Smith <dsmith@gwu.edu>
2020-08-12 16:07:54 -0400

    Alma API requests

--- alma_tests.py
+++ alma_tests.py
@@ -3,6 +3,8 @@ from alma_requests import AlmaRequests
 
 
 if __name__ == '__main__':
+	# Run from the command line to test the alma_requests module. Pass a space delimited list of GWID numbers to retrieve their barcodes.
+
 	parser = argparse.ArgumentParser(description='Process some GWIDs.')
 	parser.add_argument('users', nargs='+')
 	user_ids = parser.parse_args().users
