262a180b72780bf1397fc4b4535778a5af368020
Dolsy Smith <dsmith@gwu.edu>
2020-08-17 16:29:40 -0400

    Working version of application, minus PassagePoint API calls.

--- alma_requests.py
+++ alma_requests.py
@@ -22,15 +22,14 @@ class AlmaRequests():
 
     def _extract_barcodes(self, users: List):
         '''Given a list of user objects, extract a mapping from primary ID to barcode.'''
-        mapping = []
+        mapping = {}
         for user in users:
             primary_id = user['primary_id']
             idents = user['user_identifier']
             for ident in idents:    # Each user has more than one identifier
                 if ident['id_type']['value'] == 'BARCODE':
                     barcode = ident['value']
-                    mapping.append({'primary_id': primary_id,
-                                    'barcode': barcode})
+                    mapping[primary_id] = barcode
                     break # Once we've found the barcode, move to the next user
         return mapping
 
